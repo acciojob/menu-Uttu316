04ea994d959d6110a1ffae6cd5db32aa3162752d
Uttu316 <34169292+Uttu316@users.noreply.github.com>
2024-12-26 20:33:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,47 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import { useState } from "react";
+import { MENU_DATA } from "../../utils/menuData";
 
+const Menu = () => {
+  const [list, setlist] = useState(MENU_DATA);
+
+  const onFilter = (category) => {
+    if (category === "all") {
+      setlist(MENU_DATA);
+    } else {
+      const matches = MENU_DATA.filter((i) => i.category === category);
+
+      setlist(matches);
+    }
+  };
+  return (
+    <div id="menu">
+      <h1>Our Menu</h1>
+
+      <div>
+        <button onClick={() => onFilter("all")}>All</button>
+        <button onClick={() => onFilter("breakfast")}>Breakfast</button>
+        <button onClick={() => onFilter("lunch")}>Lunch</button>
+        <button onClick={() => onFilter("shakes")}>Shakes</button>
+      </div>
+
+      <div>
+        {list.map((i) => (
+          <div key={i.id}>
+            <div>
+              <img src={i.img} alt={i.title} />
+            </div>
+            <div>
+              <div>
+                <span>{i.title}</span>
+                <span>{i.price}</span>
+              </div>
+              <div>{i.desc}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
